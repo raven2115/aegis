eb10973312e8e2fd659dca96e6b9152333d77001
raven2115 <bsgdudheus@gmail.com>
2026-06-29 13:45:12 -0400

    Poczatek loggera

--- logger/logger.c
+++ logger/logger.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "logger.h"
 
-int main() {
+Logger logger;
+
+void Logger_init(Logger* logger, const char* path) {
+
+}
+void Logger_log(Logger* logger, const char* message) {
+
+}
+void Logger_destroy(Logger* logger) {
+
+}
+void Timestamp_format(Logger* logger) {
 
 }
--- logger/logger.h
+++ logger/logger.h
@@ -1,13 +1,17 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 #include "../shared/config.h"
+#include "../shared/event.h"
 
 typedef struct {
-    char log_path[PATH_LENGTH]; //definiujemy tablice o dlugosci 256 z configu
+    char log_path[PATH_LENGTH]; //sciezka do pliku loga gdzie bedziemy wpisywac (PATH_LENGTH = 256 z configu)
+    Event event; //event ktory bedziemy wpisywac do loga
+    FILE* log_file;
 } Logger;
 
-void Logger_init(Logger* logger, const char* path);
+void Logger_init(Logger* logger, const char* log_path);
 void Logger_log(Logger* logger, const char* message);
 void Logger_destroy(Logger* logger);
+void Timestamp_format(Logger* logger);
 
 #endif /* LOGGER_H */
