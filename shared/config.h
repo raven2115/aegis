4eac14f1fc5387d82fcc0e1269710ae37b7d1805
raven2115 <bsgdudheus@gmail.com>
2026-06-29 13:29:29 -0400

    literowki

--- shared/config.h
+++ shared/config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define PATH_LENGTH 256 //opath_length = 256 dla wszystkich co mama ten plik w importach
+#define PATH_LENGTH 256 //path_length = 256 (dla wszystkich co maja ten plik w include)
 
 #endif /* CONFIG_H */
